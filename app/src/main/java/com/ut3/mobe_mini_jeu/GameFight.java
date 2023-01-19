c23e04e25eac760f04da6401908e5c12043f4751
AZIZEN Florian <florian.azizen@master-developpement-logiciel.fr>
2023-01-19 11:07:36 +0100

    Creation of the others activities

---
+++ app/src/main/java/com/ut3/mobe_mini_jeu/GameFight.java
@@ -0,0 +1,14 @@
+package com.ut3.mobe_mini_jeu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameFight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_fight);
+    }
+}
