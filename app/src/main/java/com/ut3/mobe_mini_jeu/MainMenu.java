ce8a08d4d0aee0794fb5f17533a37cf07544491e
Dylan Caron <dylan.caron@master-developpement-logiciel.fr>
2023-01-26 11:34:33 +0100

    Link between main->navigation and main->score

--- app/src/main/java/com/ut3/mobe_mini_jeu/MainMenu.java
+++ app/src/main/java/com/ut3/mobe_mini_jeu/MainMenu.java
@@ -2,13 +2,25 @@ package com.ut3.mobe_mini_jeu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startNavigationGameActivity(View view) {
+        Intent intent = new Intent(this, NavigationGame.class);
+        startActivity(intent);
+    }
+
+    public void startScoreBoardActivity(View view) {
+        Intent intent = new Intent(this, ScoreBoard.class);
+        startActivity(intent);
     }
 }
