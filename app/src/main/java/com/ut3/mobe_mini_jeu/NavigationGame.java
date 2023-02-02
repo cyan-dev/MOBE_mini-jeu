326ffc25deabc477a4b29ae344d59afab325fa5b
AZIZEN Florian <florian.azizen@master-developpement-logiciel.fr>
2023-02-02 12:12:21 +0100

    Creation of the navigation compass

--- app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
+++ app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
@@ -2,13 +2,129 @@ package com.ut3.mobe_mini_jeu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class NavigationGame extends AppCompatActivity {
+public class NavigationGame extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager = null;
 
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
+
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+
+    private TextView labelNord;
+
+    private TextView labelX;
+    private TextView labelY;
+    private TextView labelZ;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_game);
+
+        labelNord = (TextView) findViewById(R.id.textNord);
+        labelX = (TextView) findViewById(R.id.textX);
+        labelY = (TextView) findViewById(R.id.textY);
+        labelZ = (TextView) findViewById(R.id.textZ);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //Log.d("MOBE", "SensorChanged");
+        int sensor = event.sensor.getType();
+        float [] values = event.values;
+
+        synchronized (this){
+                //Accelerometer
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                System.arraycopy(event.values, 0, accelerometerReading,
+                        0, accelerometerReading.length);
+                //Magnetic field
+            } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                System.arraycopy(event.values, 0, magnetometerReading,
+                        0, magnetometerReading.length);
+
+            }
+        }
+
+        updateOrientationAngles();
+
+        float xDeg = (float)(Math.toDegrees(orientationAngles[0])+360)%360;
+        float yDeg = (float)(Math.toDegrees(orientationAngles[1])+360)%360;
+        float zDeg = (float)(Math.toDegrees(orientationAngles[2])+360)%360;
+
+
+
+        labelX.setText("x = " + xDeg);
+        labelY.setText("y = " + yDeg);
+        labelZ.setText("Z = " + zDeg);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        //
+        // In this example, the sensor reporting delay is small enough such that
+        // the application receives an update before the system checks the sensor
+        // readings again.
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magneticField != null) {
+            sensorManager.registerListener(this, magneticField,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener((SensorEventListener) this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Don't receive any more updates from either sensor.
+        sensorManager.unregisterListener(this);
+    }
+
+    // Compute the three orientation angles based on the most recent readings from
+    // the device's accelerometer and magnetometer.
+    public void updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(rotationMatrix, null,
+                accelerometerReading, magnetometerReading);
+
+        // "rotationMatrix" now has up-to-date information.
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+
+        // "orientationAngles" now has up-to-date information.
     }
 }
