934d8cee0aa0e9d4d558064be1a08f2851743d8f
Dylan Caron <dylan.caron@master-developpement-logiciel.fr>
2023-02-16 11:36:31 +0100

    Update navigation game

--- app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
+++ app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
@@ -4,11 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class NavigationGame extends AppCompatActivity {
+    public enum Direction {
+        N, S, E, W, NE, NW, SE, SW;
+    }
+
+    private class Boat {
+        private Direction dir = Direction.N;
+        private double speed = 10.0; // en m/s
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(double speed) {
+            this.speed = speed;
+        }
+
+        public Direction getDir() {
+            return dir;
+        }
+
+        public void setDir(Direction dir) {
+            this.dir = dir;
+        }
+    }
+
+    private class Treasure {
+        private Direction dir;
+        private double dist;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_navigation_game);
     }
 }
