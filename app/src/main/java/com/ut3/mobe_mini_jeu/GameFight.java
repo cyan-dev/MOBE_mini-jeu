74bfff8428e8764aebdcb96ae570e1de99c766d7
AZIZEN Florian <florian.azizen@master-developpement-logiciel.fr>
2023-02-16 12:00:25 +0100

    Progress bar set to full hp

--- app/src/main/java/com/ut3/mobe_mini_jeu/GameFight.java
+++ app/src/main/java/com/ut3/mobe_mini_jeu/GameFight.java
@@ -3,12 +3,33 @@ package com.ut3.mobe_mini_jeu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class GameFight extends AppCompatActivity {
+public class GameFight extends AppCompatActivity implements View.OnTouchListener {
+    ProgressBar LifePoint;
+    int LifePointEnemy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_fight);
+
+        ProgressBar LifePoint = (ProgressBar) this.findViewById(R.id.hpBar);
+        LifePoint.setProgress(100);
+
+
+
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        LifePoint.setProgress(LifePoint.getProgress() - 10);
+        return true;
     }
 }
