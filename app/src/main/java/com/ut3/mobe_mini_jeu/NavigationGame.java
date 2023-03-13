ddbadf5698c2185b60337905f7d24c455e8d5e17
AZIZEN Florian <florian.azizen@master-developpement-logiciel.fr>
2023-03-13 16:46:27 +0100

    Cursor set visible depending of the direction of the treasure

--- app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
+++ app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
@@ -7,17 +7,65 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class NavigationGame extends AppCompatActivity {
     enum Direction {
-        N, S, E, W, NE, NW, SE, SW;
+        N, S, E, O, NE, NO, SE, SO;
 
         static Direction rand() {
-            return Direction.values()[(new Random()).nextInt(9)];
+            return Direction.values()[(new Random()).nextInt(8)];
+        }
+
+        public String toString(){
+            switch (this){
+                case N:
+                    return "N";
+                case S:
+                    return "S";
+                case E:
+                    return "E";
+                case O:
+                    return "O";
+                case NE:
+                    return "NE";
+                case NO:
+                    return "NO";
+                case SE:
+                    return "SE";
+                case SO:
+                    return "SO";
+                default:
+                    return "NO DIRECTION";
+            }
+        }
+
+        public int getValue(){
+            switch (this){
+                case N:
+                    return 0;
+                case S:
+                    return 4;
+                case E:
+                    return 6;
+                case O:
+                    return 2;
+                case NE:
+                    return 7;
+                case NO:
+                    return 1;
+                case SE:
+                    return 5;
+                case SO:
+                    return 3;
+                default:
+                    return -1;
+            }
         }
     }
 
@@ -78,19 +126,48 @@ public class NavigationGame extends AppCompatActivity {
         }
 
         public void generate() {
-            this.dir = Direction.N;
+            this.dir = Direction.rand();
             this.dist = (new Random()).nextInt(Treasure.MAX_DIST- Treasure.MIN_DIST)+ Treasure.MIN_DIST;
             points = this.dist;
         }
     }
 
+    //Variables
+
     private int lifePoints = 100;
     private int points = 0;
     private int randomIndex = 0;
+
+    //Views
+
+    private TextView textTop;
+    private TextView textLeft;
+    private TextView textRight;
+    private ImageView cursorTop;
+    private ImageView cursorLeft;
+    private ImageView cursorRight;
+
     private MediaPlayer coinSoundPlayer;
 
-
     private void game(View shipView, Boat boat, Treasure treasure) {
+        //Cursor
+        cursorTop.setVisibility(View.INVISIBLE);
+        cursorLeft.setVisibility(View.INVISIBLE);
+        cursorRight.setVisibility(View.INVISIBLE);
+        if(boat.dir.getValue() == treasure.dir.getValue()){
+            Log.d("Cursor", "boat dir: " + boat.dir + " treasure dir: " + treasure.dir + " DEVANT");
+            cursorTop.setVisibility(View.VISIBLE);
+        }else if((boat.dir.getValue()+1)%8 == treasure.dir.getValue() ||
+                (boat.dir.getValue()+2)%8 == treasure.dir.getValue() ||
+                (boat.dir.getValue()+3)%8 == treasure.dir.getValue()
+        ){
+            Log.d("Cursor", "boat dir: " + boat.dir + " treasure dir: " + treasure.dir + " GAUCHE");
+            cursorLeft.setVisibility(View.VISIBLE);
+        }else{
+            Log.d("Cursor", "boat dir: " + boat.dir + " treasure dir: " + treasure.dir + " DROITE");
+            cursorRight.setVisibility(View.VISIBLE);
+        }
+
         boolean isFight = updateEnemyFight();
         System.out.println(randomIndex);
         if (!isFight) {
@@ -175,9 +252,22 @@ public class NavigationGame extends AppCompatActivity {
         }
     }
 
+    private void setUpViews(){
+        //TextView
+        textTop = (TextView)  this.findViewById(R.id.textTop);
+        textLeft = (TextView)  this.findViewById(R.id.textLeft);
+        textRight = (TextView)  this.findViewById(R.id.textRight);
+
+        //ImageView
+        cursorTop = (ImageView)  this.findViewById(R.id.cursorTop);
+        cursorLeft = (ImageView)  this.findViewById(R.id.cursorLeft);
+        cursorRight = (ImageView)  this.findViewById(R.id.cursorRight);
+    }
+
     private void initializeHUD() {
         updateScore();
         updateLifePoints();
+        setUpViews();
     }
 
     @Override
