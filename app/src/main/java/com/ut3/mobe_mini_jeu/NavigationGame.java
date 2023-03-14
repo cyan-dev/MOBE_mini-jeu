fe219d1be004c32957237593eb339d1fcc9e3dad
AZIZEN Florian <florian.azizen@master-developpement-logiciel.fr>
2023-03-14 11:14:59 +0100

    Test integration of the compass

--- app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
+++ app/src/main/java/com/ut3/mobe_mini_jeu/NavigationGame.java
@@ -5,6 +5,10 @@ import java.util.Random;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,7 +20,8 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class NavigationGame extends AppCompatActivity {
+public class NavigationGame extends AppCompatActivity implements SensorEventListener {
+    //Inner Class ----------------------------------------------------------------------------------
     enum Direction {
         N, S, E, O, NE, NO, SE, SO;
 
@@ -134,13 +139,19 @@ public class NavigationGame extends AppCompatActivity {
         }
     }
 
-    //Variables
+    //Variables ------------------------------------------------------------------------------------
 
     private int lifePoints = 100;
     private int points = 0;
     private int randomIndex = 0;
     private boolean pressure = false;
     private boolean isFighting = false;
+
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
+
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
 
 
     //Views
@@ -153,12 +164,18 @@ public class NavigationGame extends AppCompatActivity {
     private ImageView cursorRight;
     private ImageView compass;
 
+    //Other
+
+    private SensorManager sensorManager = null;
+
     private MediaPlayer coinSoundPlayer;
 
     private Handler handler = new Handler();
 
     //Runnable to change the direction of the boat
     private Runnable compassRunnable;
+
+    //Game -----------------------------------------------------------------------------------------
 
     private void game(View shipView, Boat boat, Treasure treasure) {
         updateCursor(boat, treasure);
@@ -253,6 +270,104 @@ public class NavigationGame extends AppCompatActivity {
         treasureView.setLayoutParams(params);
     }
 
+    //Compass --------------------------------------------------------------------------------------
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //Log.d("MOBE", "SensorChanged");
+        int sensor = event.sensor.getType();
+        float [] values = event.values;
+
+        synchronized (this){
+            //Accelerometer
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                System.arraycopy(event.values, 0, accelerometerReading,
+                        0, accelerometerReading.length);
+                //Magnetic field
+            } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                System.arraycopy(event.values, 0, magnetometerReading,
+                        0, magnetometerReading.length);
+
+            }
+        }
+
+        updateOrientationAngles();
+
+        int xDeg = (int) (Math.toDegrees(orientationAngles[0])+360)%360;
+        int yDeg = (int)(Math.toDegrees(orientationAngles[1])+360)%360;
+        int zDeg = (int)(Math.toDegrees(orientationAngles[2])+360)%360;
+
+        //Smooth values
+        xDeg /= 45;
+        yDeg /= 45;
+        zDeg /= 45;
+
+        Log.d("Compass", "onSensorChanged: x = " + xDeg + " y = " + yDeg + " z = " + zDeg);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        //
+        // In this example, the sensor reporting delay is small enough such that
+        // the application receives an update before the system checks the sensor
+        // readings again.
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magneticField != null) {
+            sensorManager.registerListener(this, magneticField,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener((SensorEventListener) this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Don't receive any more updates from either sensor.
+        sensorManager.unregisterListener(this);
+    }
+
+    // Compute the three orientation angles based on the most recent readings from
+    // the device's accelerometer and magnetometer.
+    public void updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(rotationMatrix, null,
+                accelerometerReading, magnetometerReading);
+
+        // "rotationMatrix" now has up-to-date information.
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+
+        // "orientationAngles" now has up-to-date information.
+    }
+
+    //Initialisation -------------------------------------------------------------------------------
+
+    private void initializeHUD() {
+        updateScore();
+        updateLifePoints();
+        setUpViews();
+    }
+
     private void initializeSharedPreferences() {
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
@@ -306,12 +421,6 @@ public class NavigationGame extends AppCompatActivity {
         compass = (ImageView) this.findViewById(R.id.compass);
     }
 
-    private void initializeHUD() {
-        updateScore();
-        updateLifePoints();
-        setUpViews();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -320,6 +429,8 @@ public class NavigationGame extends AppCompatActivity {
 
         initializeSharedPreferences();
         initializeHUD();
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         setUpCompass();
         setUpCompassRunnable();
